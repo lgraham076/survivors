32f568a6ef97c726a334d92734f928f01780c575
lgraham076 <langstongraham@gmail.com>
2025-02-03 22:31:01 -0600

    Added quit option

--- scripts/game.gd
+++ scripts/game.gd
@@ -15,4 +15,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("Quit"):
+		get_tree().quit()
