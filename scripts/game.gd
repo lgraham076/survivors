66250fa093f3f6a10ab894038c5bd105f9aa28de
lgraham076 <langstongraham@gmail.com>
2025-02-06 22:07:29 -0600

    Adding continuous chunk loading

--- scripts/game.gd
+++ scripts/game.gd
@@ -13,10 +13,6 @@ func _ready() -> void:
 	$Player.position = Vector2(0,0)
 	chunk_location = Vector2(int($Player.position.x)/128, int($Player.position.y)/128)
 	
-	generate_tiles()
-	
-	
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Quit"):
@@ -26,18 +22,20 @@ func _process(delta: float) -> void:
 		
 	#print(chunk_location)
 	
-	#generate_tiles()
+	generate_tiles()
 		
 
 func generate_tiles() -> void:
+	for child in $LoadedChunks.get_children():
+		$LoadedChunks.remove_child(child)
+		child.queue_free()
+	
 	for x in range(-2, 3):
 		for y in range(-2, 3):
 			var chunk = preloader.get_resource("chunk").instantiate()
 			var chunk_size = chunk.CHUNK_SIZE.x *2
 			var chunk_x = (chunk_location.x + x) * chunk_size
 			var chunk_y = (chunk_location.y + y) * chunk_size
-			
-			print("(" + str(chunk_x) + "," + str(chunk_y) + ")")
 			
 			chunk.position = Vector2(chunk_x, chunk_y)
 			$LoadedChunks.add_child(chunk)
