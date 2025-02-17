8bbd915374a81296b3bb1668a3bab71c3e6ee134
lgraham076 <langstongraham@gmail.com>
2025-02-17 11:24:49 -0600

    Fixed tile loading

--- scripts/game.gd
+++ scripts/game.gd
@@ -8,22 +8,36 @@ var chunk_location := Vector2(0,0)
 
 var chunks = []
 
+func get_current_location():
+	
+	var x = int($Player.position.x)
+	var y = int($Player.position.y)
+	
+	if x < 0:
+		x -= 256
+	if y < 0:
+		y -= 256
+	
+	return Vector2(x/256, y/256)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Player.position = Vector2(0,0)
-	chunk_location = Vector2(int($Player.position.x)/128, int($Player.position.y)/128)
+	$Player.position = Vector2(128,128)
+	chunk_location = get_current_location()
+	print(chunk_location)
+	generate_tiles()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
+		return
 	
-	chunk_location = Vector2(int($Player.position.x)/128, int($Player.position.y)/128)
-		
-	#print(chunk_location)
-	
-	generate_tiles()
-		
+	var current_location = get_current_location()
+	if current_location != chunk_location:
+		chunk_location = current_location
+		generate_tiles()
+		print(chunk_location)	
 
 func generate_tiles() -> void:
 	for child in $LoadedChunks.get_children():
@@ -33,11 +47,10 @@ func generate_tiles() -> void:
 	for x in range(-2, 3):
 		for y in range(-2, 3):
 			var chunk = preloader.get_resource("chunk").instantiate()
-			var chunk_size = chunk.CHUNK_SIZE.x *2
+			var chunk_size = chunk.CHUNK_SIZE.x
 			var chunk_x = (chunk_location.x + x) * chunk_size
 			var chunk_y = (chunk_location.y + y) * chunk_size
-			
+
 			chunk.position = Vector2(chunk_x, chunk_y)
 			$LoadedChunks.add_child(chunk)
-	
 	
